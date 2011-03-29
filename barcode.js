4e216bb606379a171832a261fbc14ae985ca845d
Steve Spencer <steve@bigfrog.net>
2011-03-29 07:49:46 -0700

    render into canvas

--- barcode.js
+++ barcode.js
@@ -1,5 +1,5 @@
 // UPC Info: http://en.wikipedia.org/wiki/Universal_Product_Code
-function UpcEncoder() {
+var UPC = (function UPC() {
     // Digits 0 - 9: left side, right side
     var Encoding = [
         "0001101", "1110010",  // 0
@@ -16,25 +16,58 @@ function UpcEncoder() {
     // bar code: START (LLLLLL) MIDDLE (RRRRRR) END
     var Wall = ["000000000101", "01010", "101000000000"];
 
-    // 113 bits per 12 digit upc code (must be 12 digits)
-    return function(str) {
-        // ***** Pre Condition - UPC is string of 12 digits ******
-        var i, left = "", right = "", digit;
+    return {
+        bits: function(upc_code) {
+            // ***** Pre Condition - UPC is string of 12 digits ******
+            var i, left = "", right = "", digit;
 
-        for (i = 0; i < 12; i++) {
-            digit = str.charCodeAt(i) - 48;
-            if (i < 6) {
-                left += Encoding[digit * 2];
-            } else {
-                right += Encoding[digit * 2 + 1];
+            for (i = 0; i < 12; i++) {
+                digit = upc_code.charCodeAt(i) - 48;
+                if (i < 6) {
+                    left += Encoding[digit * 2];
+                } else {
+                    right += Encoding[digit * 2 + 1];
+                }
             }
+
+            return Wall[0] + left + Wall[1] + right + Wall[2];            
+        },
+        
+        render: function(canvas_name, upc_code, width) { 
+            var i, len, x, y, bits;
+            bits = this.bits(upc_code);
+            len = bits.length;
+            console.log(bits);
+            y = 120;
+            canvas = document.getElementById(canvas_name);
+            if (canvas.getContext) {    
+                context = canvas.getContext('2d'); 
+                context.lineWidth=width;
+                for (i = 0; i < len; i++) {
+                   if (bits[i] == '1') {
+                       context.strokeStyle='#000';
+                       context.beginPath();
+                       x = i * width;
+                       context.moveTo(x, 0);
+                       context.lineTo(x, y);
+                       context.stroke(); 
+                   } else {
+                       context.strokeStyle='#eee';
+                       context.beginPath();
+                       x = i * width;
+                       context.moveTo(x, 0);
+                       context.lineTo(x, y);
+                       context.stroke(); 
+                   }
+                }
+             }
         }
-
-        return Wall[0] + left + Wall[1] + right + Wall[2];
     };
-}
+})();
 
-var encode = UpcEncoder();
+// to test in node
+//    console.log(UPC.bits("785247125975"));
 
-var upc = "785247125975";
-console.log("UPC=" + encode(upc));
+
+
+
